c013731e4e645cef4eae781523263d7af04454e3
Saloni Mishra <149179860+SaloniMishra25@users.noreply.github.com>
2025-04-22 13:24:32 +0530

    Update TagBadge.tsx

--- src/component/TagBadge/TagBadge.tsx
+++ src/component/TagBadge/TagBadge.tsx
@@ -6,6 +6,7 @@ const variantMap = {
   success: "bg-green-100 text-green-800",
   warning: "bg-yellow-100 text-yellow-800",
   error: "bg-red-100 text-red-800",
+  neutral:"bg-gray-100 text-gray-800"
 };
 
 const TagBadge: React.FC<TagBadgeProps> = ({ label, variant = "primary" }) => {
